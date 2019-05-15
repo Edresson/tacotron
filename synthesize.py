e2477741b3af170c6ae0e1b3bd3df2e00c61f559
dresson <edresson1@gmail.com>
2019-05-15 13:45:50 -0300

    update

--- synthesize.py
+++ synthesize.py
@@ -22,23 +22,9 @@ import numpy as np
 import os
 from matplotlib import pylab as plt
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"  #force use CPU
-def plot_alignment(alignment, gs):
-    """Plots the alignment
-    alignments: A list of (numpy) matrix of shape (encoder_steps, decoder_steps)
-    gs : (int) global step
-    """
-    fig, ax = plt.subplots()
-    im = ax.imshow(alignment)
-
-    # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-    fig.colorbar(im)
-    plt.title('{} Steps'.format(gs))
-    plt.savefig('{}/alignment_{}k.png'.format(hp.sampledir, gs//1000), format='png')
-    
 def plot_alignment_with_text(alignment,text, info=None):
     fig, ax = plt.subplots(figsize=(16, 10))
-    im = ax.imshow(
-        alignment.T, aspect='auto', origin='lower', interpolation=None)
+    im = ax.imshow(alignment, aspect='auto', origin='lower', interpolation=None)
     fig.colorbar(im, ax=ax)
     xlabel = 'Decoder timestep'
     if info is not None:
@@ -68,14 +54,14 @@ def synthesize():
         ## mel
         y_hat = np.zeros((texts.shape[0], 200, hp.n_mels*hp.r), np.float32)  # hp.n_mels*hp.r
         for j in tqdm.tqdm(range(200)):
-            _y_hat,al = sess.run([g.y_hat,g.alignments], {g.x: texts, g.y: y_hat})
+            _y_hat,aligs = sess.run([g.y_hat,g.alignments], {g.x: texts, g.y: y_hat})
             y_hat[:, j, :] = _y_hat[:, j, :]
+
+        #plot aligments
+        for i,al in enumerate(aligs):
+            fig = plot_alignment_with_text(al[0],'A inauguração da vila é quarta ou quinta-feira')
+            fig.savefig(os.path.join(hp.sampledir,'align_'+str(i)+'_val.png'))
         ## mag
-        fig = plot_alignment_with_text(al[0],'A inauguração da vila é quarta ou quinta-feira')
-        fig.savefig(os.path.join(hp.sampledir,'align_1_val.png'))
-        plot_alignment(al[0], 123454566)
-        np.save(os.path.join(hp.sampledir,'align_save.npy'),al[0])
-        print('figura salva')
         mags = sess.run(g.z_hat, {g.y_hat: y_hat})
         for i, mag in enumerate(mags):
             print("File {}.wav is being generated ...".format(i+1))
