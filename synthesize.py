b47f6bd0941f7a0c45797b9dfcae3d10df7bb261
dresson <edresson1@gmail.com>
2019-05-15 14:55:33 -0300

    update

--- synthesize.py
+++ synthesize.py
@@ -17,35 +17,47 @@ from scipy.io.wavfile import write
 import os
 import numpy as np
 
-from matplotlib import pylab as plt
+
+
+frases=["A inauguração da vila é quarta ou quinta-feira",
+        "Vote se você tiver o título de eleitor",
+        "Hoje é fundamental encontrar a razão da existência humana",
+        "A temperatura é mais amena à noite",
+        "Em muitas cidades a população está diminuindo.",
+        "Nunca se deve ficar em cima do morro",
+        "Para as pessoas estranhas o panorama é desolador",
+        "É bom te ver colhendo flores menino",
+        "Eu finjo me banhar num lago ao amanhecer",
+        "Sua sensibilidade mostrará o caminho",
+        "A Amazônia é a reserva ecológica do globo",
+        "O ministério mudou demais com a eleição",
+        "Novas metas surgem na informática",
+        "O capital de uma empresa depende de sua produção",
+        "Se não fosse ela tudo teria sido melhor",
+        "A principal personagem no filme é uma gueixa",
+        "Espere seu amigo em casa",
+        "A juventude tinha que revolucionar a escola",
+        " A cantora terá quatro meses para ensaiar seu canto",
+        "Esse tema foi falado no congresso."]
+
 
 import os
+from matplotlib import pylab as plt
+
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"  #force use CPU
 
-def plot_alignment(alignment, gs):
-    """Plots the alignment
-    alignments: A list of (numpy) matrix of shape (encoder_steps, decoder_steps)
-    gs : (int) global step
-    """
-    fig, ax = plt.subplots()
-    im = ax.imshow(alignment)
-
-    # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-    fig.colorbar(im)
-    plt.title('{} Steps'.format(gs))
-    plt.savefig('{}/alignment_{}k.png'.format(hp.sampledir, gs//1000), format='png')
-    
 def plot_alignment_with_text(alignment,text, info=None):
     fig, ax = plt.subplots(figsize=(16, 10))
-    im = ax.imshow(
-        alignment.T, aspect='auto', origin='lower', interpolation=None)
+    im = ax.imshow(alignment, aspect='auto', origin='lower', interpolation=None)
     fig.colorbar(im, ax=ax)
     xlabel = 'Decoder timestep'
     if info is not None:
         xlabel += '\n\n' + info
-    plt.xlabel(xlabel)
-    plt.ylabel('Encoder timestep')
-    plt.yticks(range(len(text)), list(text))
+    plt.xlabel(xlabel,fontsize = 'x-large')
+    plt.ylabel('Encoder timestep',fontsize = 'x-large')
+    #plt.yticks(range(len(text)), list(text.upper()),fontsize ='xx-large' )
+    ax.set_yticks(range(len(text)))
+    ax.set_yticklabels(list(text.upper()),fontsize ='medium')
     plt.tight_layout()
     return fig
 
@@ -67,23 +79,20 @@ def synthesize():
         # Feed Forward
         ## mel
         y_hat = np.zeros((texts.shape[0], 200, hp.n_mels*hp.r), np.float32)  # hp.n_mels*hp.r
-        
         for j in tqdm.tqdm(range(200)):
-            _y_hat,al = sess.run([g.y_hat,g.alignments], {g.x: texts, g.y: y_hat})
+            _y_hat,aligs = sess.run([g.y_hat,g.alignments], {g.x: texts, g.y: y_hat})
             y_hat[:, j, :] = _y_hat[:, j, :]
-        fig = plot_alignment_with_text(al[0],'A inauguração da vila é quarta ou quinta-feira')
-        fig.savefig(os.path.join(hp.sampledir,'align_1_val.png'))
-        plot_alignment(al[0], 123454566)
-        print('figura salva')
+
+        #plot aligments
+        for i,al in enumerate(aligs):
+            fig = plot_alignment_with_text(al,frases[i])
+            fig.savefig(os.path.join(hp.sampledir,'align_'+str(i+1)+'_val.png'))
         ## mag
         mags = sess.run(g.z_hat, {g.y_hat: y_hat})
-        
-        
         for i, mag in enumerate(mags):
             print("File {}.wav is being generated ...".format(i+1))
             audio = spectrogram2wav(mag)
             write(os.path.join(hp.sampledir, '{}.wav'.format(i+1)), hp.sr, audio)
-        
         
 if __name__ == '__main__':
     synthesize()
