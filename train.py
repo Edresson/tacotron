8c16ca4d1f20a6665c45590a5440e9a6f087300b
dresson <edresson1@gmail.com>
2019-07-14 13:33:29 -0300

    update

--- train.py
+++ train.py
@@ -105,11 +105,13 @@ class Graph:
          
 if __name__ == '__main__':
     g = Graph(); print("Training Graph loaded")
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
     if( hp.TL):
         variables_to_restore = tf.contrib.framework.get_variables_to_restore(exclude=['embedding/lookup_table:0','embedding/lookup_table/Adam:0','embedding/lookup_table/Adam_1:0'])
     else:
         variables_to_restore = tf.contrib.framework.get_variables_to_restore()
-    tf.variables_initializer(tf.contrib.framework.get_variables_to_restore(), name='init')
+    
     saver = tf.train.Saver(var_list=variables_to_restore)
     sv = tf.train.Supervisor(logdir=hp.logdir, save_summaries_secs=60, save_model_secs=0,saver=saver)
 
